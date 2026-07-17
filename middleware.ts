37484433566fb4de95d9ac7019bfa9767b67c9e1
Victor-Wynter <victor@greens-corp.com>
2026-07-17 00:21:37 -0300

    teste commit

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,20 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+// import NextAuth from "next-auth";
+// import { authConfig } from "./auth.config";
 
-export default NextAuth(authConfig).auth;
+// export default NextAuth(authConfig).auth;
+
+// export const config = {
+//   matcher: ["/admin/:path*"],
+// };
+
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  return new NextResponse("Site temporariamente indisponível.", {
+    status: 503,
+  });
+}
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: "/:path*",
 };
